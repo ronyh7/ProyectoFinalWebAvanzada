a7600c0fe9463ed3da6ef6eeeda1b437867c2be3
ronyh7 <rony-hernandez_7@hotmail.com>
2016-12-06 03:41:09 -0400

    Proyecto Final: primer commit

---
+++ src/main/java/com/progwebavanzada/ProyectoFinalWebAvanzadaApplication.java
@@ -0,0 +1,12 @@
+package com.progwebavanzada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalWebAvanzadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalWebAvanzadaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/progwebavanzada/ProyectoFinalWebAvanzadaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.progwebavanzada;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProyectoFinalWebAvanzadaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
